d25a8a1a83f58aa912dfa1add59e953794b88882
soarcreator <Sohkawara@mail.greenriver.edu>
2019-09-14 10:34:57 +0900

    Optimize main.py

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = 'http://asahi.5ch.net/test/read.cgi/newsplus/1568105477/'
+url = 'http://asahi.5ch.net/test/read.cgi/newsplus/1568417189/'
 soup = BeautifulSoup(requests.get(url).text, 'lxml')
 
 thread = soup.find('div', class_ = 'thread')
 
-d = []
-for i in range(1, 1001):
-	post = thread.find('div', id = str(i))
-	name = post.find('span', class_ = 'name').string
-	date = post.find('span', class_ = 'date').string
-	uid = post.find('span', class_ = 'uid').string.replace('ID:', '')
-	message = post.find('div', class_ = 'message').get_text('\n').strip()
-	d.append({'name': name, 'date': date, 'uid': uid, 'message': message})
+for child in thread.children:
+	if not 'id' in child.attrs:
+		continue
+	i = child['id']
+	if not i.isdigit():
+		continue
+	name = child.find('span', class_ = 'name').string
+	date = child.find('span', class_ = 'date').string
+	uid = child.find('span', class_ = 'uid').string.replace('ID:', '')
+	message = child.find('div', class_ = 'message').get_text('\n').strip()
+	print(i, name, date, uid)
+	print(message)
+
+# 	d.append({'name': name, 'date': date, 'uid': uid, 'message': message})
